1a71ce711611c204e46c9caf7777f9269982bc59
Anna Kiefer <annaskiefer@gmail.com>
2016-06-19 15:43:03 -0700

    practicing recursion

--- practice.py
+++ practice.py
@@ -14,4 +14,16 @@ def reverse(str):
     	new_str += str[i-1]
     return new_str
 
+def count_list(lst):
+    """Counts a list recursively
+
+    >>> count_list([3, 6, 8, 2])
+    4
+
+    >>> count_list([])
+    0
+    """
+    if not lst:
+        return 0
+    return 1 + count_list(lst[1:])
 
